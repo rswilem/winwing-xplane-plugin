a88733bca0176aa32dd8d0d182386c9419836b00
Ramon <theramon@gmail.com>
2025-08-06 20:51:44 +0200

    fix: Fix for windows build error

--- src/include/products/fcu-efis/profiles/toliss-fcu-efis-profile.cpp
+++ src/include/products/fcu-efis/profiles/toliss-fcu-efis-profile.cpp
@@ -8,6 +8,7 @@
 #include <sstream>
 #include <iomanip>
 #include <bitset>
+#include <cmath>
 
 TolissFCUEfisProfile::TolissFCUEfisProfile(ProductFCUEfis *product) : FCUEfisAircraftProfile(product) {
     
