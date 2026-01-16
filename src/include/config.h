12168e5c3f614eba3725dc3d6253f663f821ea48
Ramon <theramon@gmail.com>
2026-01-16 15:42:32 +0100

    chore: Bump version number

--- src/include/config.h
+++ src/include/config.h
@@ -38,7 +38,7 @@
 
 #define PRODUCT_NAME "winctrl"
 #define FRIENDLY_NAME "WINCTRL"
-#define VERSION "0.0.29"
+#define VERSION "0.0.30"
 #define ALL_PLUGINS_DIRECTORY "/Resources/plugins/"
 #define PLUGIN_DIRECTORY ALL_PLUGINS_DIRECTORY PRODUCT_NAME
 #define BUNDLE_ID "com.ramonster." PRODUCT_NAME
